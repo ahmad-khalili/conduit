02d5fd928281e3171efe266a736348dd809f9f32
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-09-29 16:30:44 +0300

    Create an Article DTO

---
+++ Conduit.Core/Models/ArticleDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Conduit.Core.Models;
+
+public class ArticleDto
+{
+    public string Slug => Title.Replace("%20", "-");
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Body { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool Favorited { get; set; } = false;
+    public int FavoritesCount { get; set; }
+}
