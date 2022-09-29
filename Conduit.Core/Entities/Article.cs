5d4f940f2962d3492111d4ccac43b4a111671b9d
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-09-29 14:06:15 +0300

    Add Article Entity & configure shadow properties

---
+++ Conduit.Core/Entities/Article.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Conduit.Core.Entities;
+
+public class Article
+{
+    public int ArticleId { get; set; }
+
+    [NotMapped]
+    public string Slug => Title.Replace("%20", "-");
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Body { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    [NotMapped]
+    public bool Favorited { get; set; }
+    public int FavoritesCount { get; set; }
+}
