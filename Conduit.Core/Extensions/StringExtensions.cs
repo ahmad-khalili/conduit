c541447fa01f56ecb69a6e85f88fd3006a357837
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-02 17:42:41 +0300

    Create slug converter extension method

---
+++ Conduit.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Conduit.Core.Extensions;
+
+public static class StringExtensions
+{
+    public static string ToSlug(this string source)
+    {
+        var removedSpecial = Regex
+            .Replace(source, "[^0-9A-Za-z ]+", "", RegexOptions.Compiled);
+
+        var toSlug = removedSpecial.Replace(" ", "-").ToLower();
+
+        return toSlug;
+    }
+}
