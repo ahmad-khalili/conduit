b66425fda148c5a4c5cfa44186302ca8c05b6329
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-03 16:22:02 +0300

    Create required User DTOs

---
+++ Conduit.Core/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Conduit.Core.Models;
+
+public class UserDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Token { get; set; }
+    public string Bio { get; set; }
+    public string Image { get; set; }
+}
---
+++ Conduit.Core/Models/UserForCreationDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Conduit.Core.Models;
+
+public class UserForCreationDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
