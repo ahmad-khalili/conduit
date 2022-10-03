cdd4deee0f540b506da113771bb93a3e53b51877
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-03 16:22:22 +0300

    Create User Automapper Profile

---
+++ Conduit.Core/Profiles/UserProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using Conduit.Core.Entities;
+using Conduit.Core.Models;
+
+namespace Conduit.Core.Profiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<UserForCreationDto, User>();
+        CreateMap<User, UserDto>();
+    }
+}
