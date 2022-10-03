9de14402fbbdab9755065758d925f58f9ae7b489
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-03 16:27:58 +0300

    Create Registration Validator

---
+++ Conduit.SharedKernel/Validators/UserValidator.cs
@@ -0,0 +1,15 @@
+﻿using Conduit.Core.Entities;
+using Conduit.Core.Models;
+using FluentValidation;
+
+namespace Conduit.SharedKernel.Validators;
+
+public class RegisterUserValidator : AbstractValidator<UserForCreationDto>
+{
+    public RegisterUserValidator()
+    {
+        RuleFor(user => user.UserName).NotEmpty();
+        RuleFor(user => user.Email).NotEmpty();
+        RuleFor(user => user.Password).NotEmpty();
+    }
+}
