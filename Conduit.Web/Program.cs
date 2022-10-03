c0b4ed8c48deea91a85c3b2fc827dac4ea262481
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-03 16:29:26 +0300

    Inject UserRepository & JWTBearer

--- Conduit.Web/Program.cs
+++ Conduit.Web/Program.cs
@@ -1,10 +1,13 @@
 using System.Reflection;
+using System.Text;
 using Conduit.Infrastructure;
 using Conduit.Infrastructure.Repositories;
 using Conduit.SharedKernel.Interfaces;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -21,13 +24,30 @@ builder.Services.AddDbContext<ConduitDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("ConduitDb"));
 });
 
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.SaveToken = true;
+    options.RequireHttpsMetadata = false;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidAudience = builder.Configuration["JWT:ValidAudience"],
+        ValidIssuer = builder.Configuration["JWT:ValidIssuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Secret"]))
+    };
+});
+
 builder.Services.AddAutoMapper(Assembly.Load("Conduit.Core"));
 
 builder.Services.TryAddScoped<IArticleRepository, ArticleRepository>();
 
+builder.Services.TryAddScoped<IUserRepository, UserRepository>();
+
 builder.Services.AddValidatorsFromAssembly(Assembly.Load("Conduit.SharedKernel"));
 
 var app = builder.Build();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -37,6 +57,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
