510958979eb58af1c89d6fb273113b39a176e706
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-05 14:18:59 +0300

    Make UserValidator more generic & add imageUrl rule

---
+++ Conduit.SharedKernel/Validators/UserValidator.cs
@@ -0,0 +1,19 @@
+﻿using Conduit.Core.Entities;
+using FluentValidation;
+
+namespace Conduit.SharedKernel.Validators;
+
+public class UserValidator : AbstractValidator<User>
+{
+    public UserValidator()
+    {
+        RuleFor(user => user.UserName).NotEmpty();
+        RuleFor(user => user.Email).NotEmpty();
+        RuleFor(user => user.Password).NotEmpty();
+        RuleFor(user => user.Image).Custom((image, context) =>
+        {
+            if (!image.ToLower().Contains("image"))
+                context.AddFailure("Invalid Image Url!");
+        }).When(user => user.Image != default);
+    }
+}
