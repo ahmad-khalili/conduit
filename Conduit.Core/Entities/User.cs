f935a614a5ad5734872bbf435f6906fe4bda5589
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-05 14:18:07 +0300

    Make token nullable

--- Conduit.Core/Entities/User.cs
+++ Conduit.Core/Entities/User.cs
@@ -6,7 +6,7 @@ public class User
     public string UserName { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string Token { get; set; }
+    public string? Token { get; set; }
     public string? Bio { get; set; }
     public string? Image { get; set; }
 
