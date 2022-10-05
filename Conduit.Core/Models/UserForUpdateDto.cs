9c76e407e452ecc1a956393af2aeab2e08d56dec
ahmad-khalili <ahmadkhalili01@outlook.com>
2022-10-05 14:18:25 +0300

    Create User update DTO

---
+++ Conduit.Core/Models/UserForUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Conduit.Core.Models;
+
+public class UserForUpdateDto
+{
+    public string UserName { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string? Bio { get; set; }
+    public string? Image { get; set; }
+}
